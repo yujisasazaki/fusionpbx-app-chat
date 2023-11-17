eb53a10b5d2162c93c4cf1d59105c9e80913fb8a
Jhonatan Sasazaki <jhonatan-yuji@hotmail.com>
2023-11-17 12:09:06 -0300

    Create Chatwoot app_languages.php

---
+++ chatwoot/app_languages.php
@@ -0,0 +1,23 @@
+<?php
+
+//Chatwoot API
+$text['title-chatwoot']['en-us'] = 'Chatwoot';
+$text['title-chatwoot']['en-gb'] = 'Chatwoot';
+$text['title-chatwoot']['ar-eg'] = 'Chatwoot';
+$text['title-chatwoot']['de-at'] = 'Chatwoot';
+$text['title-chatwoot']['de-ch'] = 'Chatwoot';
+$text['title-chatwoot']['de-de'] = 'Chatwoot';
+$text['title-chatwoot']['es-cl'] = 'Chatwoot';
+$text['title-chatwoot']['es-mx'] = 'Chatwoot';
+$text['title-chatwoot']['fr-ca'] = 'Chatwoot';
+$text['title-chatwoot']['fr-fr'] = 'Chatwoot';
+$text['title-chatwoot']['he-il'] = 'Chatwoot';
+$text['title-chatwoot']['it-it'] = 'Chatwoot';
+$text['title-chatwoot']['nl-nl'] = 'Chatwoot';
+$text['title-chatwoot']['pl-pl'] = 'Chatwoot';
+$text['title-chatwoot']['pt-br'] = 'Chatwoot';
+$text['title-chatwoot']['pt-pt'] = 'Chatwoot';
+$text['title-chatwoot']['ro-ro'] = 'Chatwoot';
+$text['title-chatwoot']['ru-ru'] = 'Chatwoot';
+$text['title-chatwoot']['sv-se'] = 'Chatwoot';
+$text['title-chatwoot']['uk-ua'] = 'Chatwoot';
