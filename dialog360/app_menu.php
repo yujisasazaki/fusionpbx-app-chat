2afa0b0b3fbfc2dfb92ad93219650fed30df3033
Jhonatan Sasazaki <jhonatan-yuji@hotmail.com>
2024-03-04 11:07:29 -0300

    fix: Correct 360Dialog menu path

--- dialog360/app_menu.php
+++ dialog360/app_menu.php
@@ -23,6 +23,6 @@ $apps[$x]['menu'][$y]['title']['uk-ua'] = "360dialog";
 $apps[$x]['menu'][$y]['uuid'] = "b63144f1-f8f4-48ad-8ad1-6182c57db8b3";
 $apps[$x]['menu'][$y]['parent_uuid'] = "fd29e39c-c936-f5fc-8e2b-611681b266b5";
 $apps[$x]['menu'][$y]['category'] = "internal";
-$apps[$x]['menu'][$y]['path'] = "/app/360dialog/";
+$apps[$x]['menu'][$y]['path'] = "/app/dialog360/";
 $apps[$x]['menu'][$y]['groups'][] = "superadmin";
 $apps[$x]['menu'][$y]['groups'][] = "admin";
